3e2c171e3c9fec5082ad6c0687b9ad00d4cd6ca2
nadavbara <nadavbara@gmail.com>
2016-08-26 22:53:58 +0300

    removing unnecessary lines

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 var express = require('express'),
-    https = require('https'),
-    fs = require('fs'),
 	morgan = require('morgan'),
     bodyParser      = require('body-parser'),
     methodOverride  = require('method-override'),
@@ -8,23 +6,11 @@ var express = require('express'),
     auth        = require('./routes/auth'),
     orders        = require('./routes/orders'),
     admin        = require('./routes/admin'),
-    mongoose        = require('mongoose'),
-    mongodb         = require('./dbConnections/mongoDbconnection'),
-    passport = require('passport'),
-    strategy = require('./jwt/setup-passport'),
     cookieParser = require('cookie-parser'),
-    session = require('express-session'),
     app = express();
-
-var options = {
-  key: fs.readFileSync('./ssl/key.pem'),
-  cert: fs.readFileSync('./ssl/cert.pem')
-};
 
 app.set('port', (process.env.PORT || 5000));
 
-//var url = 'mongodb://localhost:27017/crema_test_db';
-var url = 'mongodb://baza:bgu4life@ds011715.mlab.com:11715/crema_test_db';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -39,11 +25,6 @@ app.all('*', function(req, res, next) {
 });
 
 app.use(cookieParser());
-// See express session docs for information on the options: https://github.com/expressjs/session
-app.use(session({ secret: 'oijBHgmDtIME_UNusj0J_R9Tj1H_qXwKGtBG_EyO0JSU5xkjsN6WJMauBnTWForm', resave: false,  saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use('/menu', menu);
 app.use('/auth',auth);
 app.use('/orders',orders);
